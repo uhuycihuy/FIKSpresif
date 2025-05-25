7def4d0405d4fc39729cd519c96de00f7e82d0f0
uhuycihuy <ridhobujank69@gmail.com>
2025-05-25 19:09:10 +0700

    Baru activity splash screen

---
+++ app/src/main/java/com/example/fikspresif/SplashActivity.java
@@ -0,0 +1,27 @@
+package com.example.fikspresif;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+@SuppressLint("CustomSplashScreen")
+public class SplashActivity extends AppCompatActivity {
+    private int waktu_loading = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent mainIntent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(mainIntent);
+                finish();
+            }
+        }, waktu_loading);
+    }
+}
